95e2ab307b3d57efa9b4096c85f4e37ad40e6c25
owlthinkabtit <haggins.jameka@gmail.com>
2024-09-18 16:30:46 -0400

    Stopped at the filtering section with the Featured Books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
+import Highlights from './components/Highlights';
+import Featured from './components/Featured';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
