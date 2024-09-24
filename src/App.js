ecbca7ab9d76ddea903da27cc7f0263644268974
owlthinkabtit <haggins.jameka@gmail.com>
2024-09-24 19:30:50 -0400

    working on routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          {/* <Route path="/books" element={() => <Books books={books} />} />*/}
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} cart={cart} />} />
+          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} />} />
+          <Footer />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
