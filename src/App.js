932a12c4e9ffdcf4ccbba581c25bc0a8187ba72d
owlthinkabtit <haggins.jameka@gmail.com>
2024-09-26 18:38:38 -0400

    working on the cart and putting finishing touches on this project

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
 import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router>
       <div className="App">
@@ -17,9 +29,9 @@ function App() {
           <Route path="/books" element={<Books books={books} />} />
           {/* <Route path="/books" element={() => <Books books={books} />} />*/}
           <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} cart={cart} />} />
-          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} />} />
-          <Footer />
+         {/* <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} />} />*/}
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
